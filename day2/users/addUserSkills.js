f8fdafec8be6596e9b11ece054c7080d17415243
prajapatilalit <prajapatilalit150992@gmail.com>
2021-09-19 10:36:48 +0530

    day2 add user skills solution

---
+++ day2/users/addUserSkills.js
@@ -0,0 +1,61 @@
+const users = [
+  {
+    name: "Brook",
+    scores: 75,
+    skills: ["HTM", "CSS", "JS"],
+    age: 16,
+  },
+  {
+    name: "Alex",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "David",
+    scores: 75,
+    skills: ["HTM", "CSS"],
+    age: 22,
+  },
+  {
+    name: "John",
+    scores: 85,
+    skills: ["HTM"],
+    age: 25,
+  },
+  {
+    name: "Sara",
+    scores: 95,
+    skills: ["HTM", "CSS", "JS"],
+    age: 26,
+  },
+  {
+    name: "Martha",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "Thomas",
+    scores: 90,
+    skills: ["HTM", "CSS", "JS"],
+    age: 20,
+  },
+];
+
+function addUser(usersData) {
+  let name = "Thomas";
+  let findUser = usersData.find((user) => user.name === name);
+
+  if (findUser) {
+    findUser.skills.push("JQ");
+    //return only find user data
+    return findUser;
+  } else {
+    let message = "User Not Found";
+    return message;
+  }
+  //return all user data
+  //   return usersData;
+}
+console.log(addUser(users));
